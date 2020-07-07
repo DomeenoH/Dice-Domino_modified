4e064c7a528d42bf40d6ca14a0f1c89bbbc1c2ca
DomeenoH <287887313@qq.com>
2020-07-08 03:54:42 +0800

    修复了master权限 修改了闲置提醒时间 修复了stshow的bug

--- Dice/EventHandler.cpp
+++ Dice/EventHandler.cpp
@@ -322,6 +322,17 @@ namespace Dice
 			}
 		}
 		ifstreamWelcomeMsg.close();
+				ifstream ifstreamRoonRule(strFileLoc + "RoomRule.RDconf");
+		if (ifstreamRoonRule)
+		{
+			long long GroupID;
+			int RoonRuleNum;
+			while (ifstreamRoonRule >> GroupID >> RoonRuleNum)
+			{
+				RoomRule[GroupID] = RoonRuleNum;
+			}
+		}
+		ifstreamRoonRule.close();
 		ilInitList = make_unique<Initlist>(strFileLoc + "INIT.DiceDB");
 		ifstream ifstreamCustomMsg(strFileLoc + "CustomMsg.json");
 		if (ifstreamCustomMsg)
@@ -653,7 +664,7 @@ namespace Dice
 					}
 					return;
 			}
-			if (strLowerMessage.substr(intMsgCnt, 4) == "show")
+if (strLowerMessage.substr(intMsgCnt, 4) == "show")
 			{
 				intMsgCnt += 4;
 				while (isspace(static_cast<unsigned char>(strLowerMessage[intMsgCnt])))
@@ -665,24 +676,46 @@ namespace Dice
 					strSkillName += strLowerMessage[intMsgCnt];
 					intMsgCnt++;
 				}
-					if (SkillNameReplace.count(strSkillName))strSkillName = SkillNameReplace[strSkillName];
-					if (CharacterProp.count(SourceType(dice_msg.qq_id, dice_msg.msg_type, dice_msg.group_id)) && CharacterProp[SourceType(
-						dice_msg.qq_id, dice_msg.msg_type, dice_msg.group_id)].count(strSkillName))
+				if (SkillNameReplace.count(strSkillName))
+					strSkillName = SkillNameReplace[strSkillName];
+				if (PlayerGroupList.count(dice_msg.qq_id) && PlayerGroupList[dice_msg.qq_id].count(dice_msg.group_id) 
+					&& MultCharProp[dice_msg.qq_id][PlayerGroupList[dice_msg.qq_id][dice_msg.group_id]].count(strSkillName))
+				{
+					dice_msg.Reply(format(GlobalMsg["strProp"], { "[角色卡]" + PlayerGroupList[dice_msg.qq_id][dice_msg.group_id],strSkillName,
+						to_string(MultCharProp[dice_msg.qq_id][PlayerGroupList[dice_msg.qq_id][dice_msg.group_id]][strSkillName]) }));
+				}
+				else if (CharacterProp.count(SourceType(dice_msg.qq_id, dice_msg.msg_type, dice_msg.group_id)) 
+					&& CharacterProp[SourceType(dice_msg.qq_id, dice_msg.msg_type, dice_msg.group_id)].count(strSkillName))
+				{
+					dice_msg.Reply(format(GlobalMsg["strProp"], {strNickName + "的[默认卡]", strSkillName, 
+						to_string(CharacterProp[SourceType(dice_msg.qq_id, dice_msg.msg_type, dice_msg.group_id)][strSkillName])}));
+				}
+				else if (SkillDefaultVal.count(strSkillName))
+				{
+					dice_msg.Reply(format(GlobalMsg["strProp"], { strNickName, strSkillName, to_string(SkillDefaultVal[strSkillName]) }));
+				}
+				else if (!strSkillName.length())/*mark*/
+				{
+					string strReply = "多多从帽子里抽出一卷皱巴巴的羊皮纸：“" + strNickName + "都有这样的一些属性喵——”\n";
+					map<string, int> AllSkill = CharacterProp[SourceType(dice_msg.qq_id, dice_msg.msg_type, dice_msg.group_id)];
+					if (AllSkill.empty())
 					{
-						dice_msg.Reply(format(GlobalMsg["strProp"], {
-							strNickName, strSkillName,
-							to_string(CharacterProp[SourceType(dice_msg.qq_id, dice_msg.msg_type, dice_msg.group_id)][strSkillName])
-							}));
+						dice_msg.Reply(strNickName + "好像没有录入过和默认值不一样的信息喵——");
+						return;
 					}
-					else if (SkillDefaultVal.count(strSkillName))
+					map<string, int>::iterator SkillCount = AllSkill.begin();
+					while (!(SkillCount == AllSkill.end()))
 					{
-						dice_msg.Reply(format(GlobalMsg["strProp"], { strNickName, strSkillName, to_string(SkillDefaultVal[strSkillName]) }));
+						strReply = strReply + " " + SkillCount->first + to_string(SkillCount->second);
+						SkillCount++;
 					}
-					else
-					{
-						dice_msg.Reply(GlobalMsg["strPropNotFound"]);
-					}
-					return;
+					dice_msg.Reply(strReply);
+				}
+				else
+				{
+					dice_msg.Reply(GlobalMsg["strPropNotFound"]);
+				}
+				return;
 			}
 			bool boolError = false;
 			while (intMsgCnt != strLowerMessage.length())
